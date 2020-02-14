ffbe337d5751cf89efc29a559e17131247d38b46
zhangpu <791458703@qq.com>
2020-02-14 11:25:32 +0800

    init project

---
+++ fcadmin-api/src/main/java/org/fcadmin/MyApplication.java
@@ -0,0 +1,14 @@
+package org.fcadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+//扫描mybatis mapper
+//@MapperScan(basePackages = "org.fcadmin.mapper")
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+    }
+}
